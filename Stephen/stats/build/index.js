0a4778027adb9664cffbc67746c03b91b0ae8d46
SamHuang <sam821203@yahoo.com.tw>
2023-11-24 23:11:24 +0800

    complete: Stephen chapter 11-121

---
+++ Stephen/stats/build/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const MatchReader_1 = require("./MatchReader");
+const MatchResult_1 = require("./MatchResult");
+const reader = new MatchReader_1.MatchReader("football.csv");
+reader.read();
+const dateOfFirstMatch = reader.data[0][0];
+let manUnitedWins = 0;
+for (let match of reader.data) {
+    if (match[1] === "Man United" && match[5] === MatchResult_1.MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === "Man United" && match[5] === MatchResult_1.MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log(`Man United Wins: ${manUnitedWins} games`);
---
+++ Stephen/stats/build/ts.js
@@ -0,0 +1,7 @@
+"use strict";
+class HoldAnything {
+}
+const holdNumber = new HoldAnything();
+holdNumber.data = 123;
+const holdString = new HoldAnything();
+holdString.data = "hello";
